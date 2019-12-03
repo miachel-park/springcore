585a021d68e0e75c2d44b842aaacb3e44f3b9783
miachel <sangyong_park@tmax.co.kr>
2019-12-03 09:47:08 +0900

    merge

--- example01/src/main/java/pe/miachel/springcore/example13/MainClass.java
+++ example01/src/main/java/pe/miachel/springcore/example13/MainClass.java
@@ -1,32 +1,3 @@
-<<<<<<< HEAD
-package pe.miachel.springcore.example13;
-
-import org.springframework.context.support.GenericXmlApplicationContext;
-
-public class MainClass {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		GenericXmlApplicationContext ctx01 = new GenericXmlApplicationContext();
-		ctx01.load("classpath:applicationCTX13.xml");
-		ctx01.refresh();
-		
-		GenericXmlApplicationContext ctx02 = new GenericXmlApplicationContext();
-		ctx02.load("classpath:applicationCTX13.xml");
-		ctx02.refresh();
-		
-		System.out.println("Singleton myBean in container01 :" + ctx01.getBean("myBean", OneBean.class).getCreatedTime());
-		System.out.println("Singleton myBean in container01 :" + ctx01.getBean("myBean", OneBean.class).toString());
-		
-		System.out.println("Singleton myBean in container02 :" + ctx02.getBean("myBean", OneBean.class).getCreatedTime());
-		System.out.println("Singleton myBean in container02 :" + ctx02.getBean("myBean", OneBean.class).toString());
-		
-		ctx01.close();
-		ctx02.close();
-	}
-
-}
-=======
 package pe.miachel.springcore.example13;
 
 import org.slf4j.Logger;
@@ -52,4 +23,4 @@ public class MainClass {
 	}
 
 }
->>>>>>> branch 'master' of http://github.com/miachel-park/springcore
+
